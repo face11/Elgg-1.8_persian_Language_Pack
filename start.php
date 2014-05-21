81970dc4306153ea6cece587c8a930a78b6d42ec
face1 <face1@users.noreply.github.com>
2014-05-21 12:42:28 +0430

    Create start.php

---
+++ start.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * persian Language Pack for Elgg 1.8
+ *
+ * This plugin provides the persian translations for the language files of Elgg including bundled plugins.
+ *
+ * (C) face11 2014
+ * GNU Public License version 2
+ *
+ * https://github.com/face11
+ * Contact: info@facebaran.com
+ */
+
+register_translations(dirname(__FILE__) . "/languages/", true);
